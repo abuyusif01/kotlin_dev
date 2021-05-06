e86c3e0fd2995ce187fed5ec2d93fc818b0e9fe8
blackdracula18 <Abubakaryusuf951@gmail.com>
2021-05-06 12:37:19 +0800

    making the Ui

--- Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun onDigit(view: View)
+    {
+
+    }
+    fun onOperator(view: View)
+    {
+
+    }
+
 }
